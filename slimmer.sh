374c98cbeccab8426041af2a72d465455834e03d
Francesco Giannelli <bassobassista@gmail.com>
2016-06-23 09:56:21 +0000

    Update slimmer.sh

--- slimmer.sh
+++ slimmer.sh
@@ -4,5 +4,9 @@ docker build -t alpprxctr .
 
 ./docker-slim build \
 	--continue-after 30 \
+	--http-probe \
 	--include-path /entry \
-	--http-probe
+	--include-path /bin/tail \
+	--include-path /bin/sleep \
+	--include-path /bin/cp \
+	alpprxctr
